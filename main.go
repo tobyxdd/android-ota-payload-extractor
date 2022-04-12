40d5c644e5ec6f64af6ea516d1bceba7e0246315
Toby <tobyxdd@gmail.com>
2022-04-12 15:06:37 -0700

    code improvements & zero op support

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"sync"
 )
 
 const (
@@ -21,53 +20,47 @@ const (
 	payloadMagic = "CrAU"
 
 	brilloMajorPayloadVersion = 2
-
-	blockSize = uint64(4096)
 )
-
-var seekReadMutex sync.Mutex
-var wg sync.WaitGroup
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatalln("not enough arguments")
+		fmt.Printf("Usage: %s <input>\n", os.Args[0])
+		os.Exit(1)
 	}
 	filename := os.Args[1]
 	f, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("unable to open file %s: %s\n", filename, err.Error())
+		log.Fatalf("Failed to open file: %s", err)
 	}
 	if isZip(f) {
-		// extract payload.bin from the zip first
+		// Extract payload.bin from the zip first
 		_ = f.Close()
-		log.Printf("input recognized as a zip file. searching for %s ...\n", payloadFilename)
+		log.Printf("Input is a zip file, searching for %s ...\n", payloadFilename)
 		zr, err := zip.OpenReader(filename)
 		if err != nil {
-			log.Fatalf("unable to open zip file %s: %s\n", filename, err.Error())
+			log.Fatalf("Failed to open the zip file: %s\n", err.Error())
 		}
 		zf, err := findPayload(zr)
 		if err != nil {
-			log.Fatalf("unable to read from the zip file: %s\n", err.Error())
+			log.Fatalf("Failed to read from the zip file: %s\n", err.Error())
 		}
 		if zf == nil {
 			log.Fatalf("%s not found in the zip file\n", payloadFilename)
 		}
-		log.Printf("%s found, extracting...\n", payloadFilename)
-		pf, err := os.Create(payloadFilename)
+		log.Printf("Extracting %s ...\n", payloadFilename)
+		f, err = os.Create(payloadFilename)
 		if err != nil {
-			log.Fatalf("unable to create extraction file %s: %s\n", payloadFilename, err.Error())
+			log.Fatalf("Failed to create the extraction file: %s\n", err.Error())
 		}
-		_, err = io.Copy(pf, zf)
+		_, err = io.Copy(f, zf)
 		if err != nil {
-			log.Fatalf("extraction failed: %s\n", err.Error())
+			log.Fatalf("Failed to extract: %s\n", err.Error())
 		}
 		_ = zf.Close()
 		_ = zr.Close()
-		_, _ = pf.Seek(0, 0)
-		parsePayload(pf)
-	} else {
-		parsePayload(f)
+		_, _ = f.Seek(0, 0)
 	}
+	parsePayload(f)
 }
 
 func isZip(f *os.File) bool {
@@ -87,114 +80,134 @@ func findPayload(zr *zip.ReadCloser) (io.ReadCloser, error) {
 }
 
 func parsePayload(r io.ReadSeeker) {
-	log.Println("parsing payload...")
+	log.Println("Parsing payload...")
 	// magic
 	magic := make([]byte, len(payloadMagic))
 	_, err := r.Read(magic)
 	if err != nil || string(magic) != payloadMagic {
-		log.Fatalf("incorrect magic (%s)\n", string(magic))
+		log.Fatalf("Incorrect magic (%s)\n", string(magic))
 	}
 	// version & lengths
 	var version, manifestLen uint64
 	var metadataSigLen uint32
 	err = binary.Read(r, binary.BigEndian, &version)
 	if err != nil || version != brilloMajorPayloadVersion {
-		log.Fatalf("incorrect version (%d)\n", version)
+		log.Fatalf("Unsupported payload version (%d). This tool only supports version %d\n",
+			version, brilloMajorPayloadVersion)
 	}
 	err = binary.Read(r, binary.BigEndian, &manifestLen)
 	if err != nil || !(manifestLen > 0) {
-		log.Fatalf("incorrect manifest length (%d)\n", manifestLen)
+		log.Fatalf("Incorrect manifest length (%d)\n", manifestLen)
 	}
 	err = binary.Read(r, binary.BigEndian, &metadataSigLen)
 	if err != nil || !(metadataSigLen > 0) {
-		log.Fatalf("incorrect metadata signature length (%d)\n", metadataSigLen)
+		log.Fatalf("Incorrect metadata signature length (%d)\n", metadataSigLen)
 	}
-	log.Printf("version: %d, manifest length: %d, metadata signature length: %d\n",
-		version, manifestLen, metadataSigLen)
 	// manifest
 	manifestRaw := make([]byte, manifestLen)
 	n, err := r.Read(manifestRaw)
 	if err != nil || uint64(n) != manifestLen {
-		log.Fatalf("unable to read manifest (%d)\n", n)
+		log.Fatalf("Failed to read the manifest (%d)\n", manifestLen)
 	}
 	var manifest DeltaArchiveManifest
 	err = proto.Unmarshal(manifestRaw, &manifest)
 	if err != nil {
-		log.Fatalf("unable to parse manifest: %s\n", err.Error())
+		log.Fatalf("Failed to parse the manifest: %s\n", err.Error())
 	}
+	// only support full payloads!
+	if *manifest.MinorVersion != 0 {
+		log.Fatalf("Delta payloads are not supported, please use a full payload file\n")
+	}
+	// print manifest info
+	log.Printf("Block size: %d, Partition count: %d\n",
+		*manifest.BlockSize, len(manifest.Partitions))
 	// extract partitions
-	extractPartitions(manifest.Partitions, r, 24+manifestLen+uint64(metadataSigLen))
+	extractPartitions(&manifest, r, 24+manifestLen+uint64(metadataSigLen))
+	// done
+	log.Println("Done!")
 }
 
-func extractPartitions(partitions []*PartitionUpdate, r io.ReadSeeker, baseOffset uint64) {
-	log.Printf("%d partitions", len(partitions))
-	for _, p := range partitions {
+func extractPartitions(manifest *DeltaArchiveManifest, r io.ReadSeeker, baseOffset uint64) {
+	for _, p := range manifest.Partitions {
 		if p.PartitionName == nil {
 			continue
 		}
-		wg.Add(1)
-		go func(p *PartitionUpdate) {
-			outFilename := fmt.Sprintf("%s.img", *p.PartitionName)
-			extractPartition(p, outFilename, r, baseOffset)
-			log.Println(outFilename, "extracted")
-			wg.Done()
-		}(p)
+		log.Printf("Extracting %s (%d ops) ...", *p.PartitionName, len(p.Operations))
+		outFilename := fmt.Sprintf("%s.img", *p.PartitionName)
+		extractPartition(p, outFilename, r, baseOffset, *manifest.BlockSize)
 	}
-	wg.Wait()
 }
 
-func extractPartition(p *PartitionUpdate, outFilename string, r io.ReadSeeker, baseOffset uint64) {
+func extractPartition(p *PartitionUpdate, outFilename string, r io.ReadSeeker, baseOffset uint64, blockSize uint32) {
 	outFile, err := os.Create(outFilename)
 	if err != nil {
-		log.Fatalf("unable to create output file %s: %s\n", outFilename, err.Error())
+		log.Fatalf("Failed to create the output file: %s\n", err.Error())
 	}
 	for _, op := range p.Operations {
-		e := op.DstExtents[0]
 		data, dataPos := make([]byte, *op.DataLength), int64(baseOffset+*op.DataOffset)
 
-		seekReadMutex.Lock()
 		_, err = r.Seek(dataPos, 0)
 		if err != nil {
-			seekReadMutex.Unlock()
 			_ = outFile.Close()
-			log.Fatalf("unable to seek to %d in partition %s\n", dataPos, *p.PartitionName)
+			log.Fatalf("Failed to seek to %d in partition %s: %s\n", dataPos, outFilename, err.Error())
 		}
 		n, err := r.Read(data)
-		seekReadMutex.Unlock()
-
 		if err != nil || uint64(n) != *op.DataLength {
 			_ = outFile.Close()
-			log.Fatalf("unable to read enough data from partition %s\n", *p.PartitionName)
+			log.Fatalf("Failed to read enough data from partition %s: %s\n", outFilename, err.Error())
 		}
-		_, _ = outFile.Seek(int64(*e.StartBlock*blockSize), 0)
+
+		outSeekPos := int64(*op.DstExtents[0].StartBlock * uint64(blockSize))
+		_, err = outFile.Seek(outSeekPos, 0)
+		if err != nil {
+			_ = outFile.Close()
+			log.Fatalf("Failed to seek to %d in partition %s: %s\n", outSeekPos, outFilename, err.Error())
+		}
+
 		switch *op.Type {
 		case InstallOperation_REPLACE:
 			_, err = outFile.Write(data)
 			if err != nil {
 				_ = outFile.Close()
-				log.Fatalf("unable to write output to %s: %s\n", outFilename, err.Error())
+				log.Fatalf("Failed to write output to %s: %s\n", outFilename, err.Error())
 			}
 		case InstallOperation_REPLACE_BZ:
 			bzr := bzip2.NewReader(bytes.NewReader(data))
 			_, err = io.Copy(outFile, bzr)
 			if err != nil {
 				_ = outFile.Close()
-				log.Fatalf("unable to write output to %s: %s\n", outFilename, err.Error())
+				log.Fatalf("Failed to write output to %s: %s\n", outFilename, err.Error())
 			}
 		case InstallOperation_REPLACE_XZ:
 			xzr, err := xz.NewReader(bytes.NewReader(data), 0)
 			if err != nil {
 				_ = outFile.Close()
-				log.Fatalf("bad xz data in partition %s: %s\n", *p.PartitionName, err.Error())
+				log.Fatalf("Bad xz data in partition %s: %s\n", *p.PartitionName, err.Error())
 			}
 			_, err = io.Copy(outFile, xzr)
 			if err != nil {
 				_ = outFile.Close()
-				log.Fatalf("unable to write output to %s: %s\n", outFilename, err.Error())
+				log.Fatalf("Failed to write output to %s: %s\n", outFilename, err.Error())
+			}
+		case InstallOperation_ZERO:
+			for _, ext := range op.DstExtents {
+				outSeekPos = int64(*ext.StartBlock * uint64(blockSize))
+				_, err = outFile.Seek(outSeekPos, 0)
+				if err != nil {
+					_ = outFile.Close()
+					log.Fatalf("Failed to seek to %d in partition %s: %s\n", outSeekPos, outFilename, err.Error())
+				}
+				// write zeros
+				_, err = io.Copy(outFile, bytes.NewReader(make([]byte, *ext.NumBlocks*uint64(blockSize))))
+				if err != nil {
+					_ = outFile.Close()
+					log.Fatalf("Failed to write output to %s: %s\n", outFilename, err.Error())
+				}
 			}
 		default:
 			_ = outFile.Close()
-			log.Fatalf("unsupported operation type: %d (%s)\n", *op.Type, InstallOperation_Type_name[int32(*op.Type)])
+			log.Fatalf("Unsupported operation type: %d (%s), please report a bug\n",
+				*op.Type, InstallOperation_Type_name[int32(*op.Type)])
 		}
 	}
 }
